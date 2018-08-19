582346c89a0d5be60a05cf0850a371a1c294fc9b
ChkBuk <33537268+ChkBuk@users.noreply.github.com>
2018-08-19 18:31:35 +0800

    Production20180819

--- polling-app-client/src/constants/index.js
+++ polling-app-client/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8080/api';
+export const API_BASE_URL = 'http://ec2-18-218-57-96.us-east-2.compute.amazonaws.com:8080/api';
 //export const API_BASE_URL = '/api';
 export const ACCESS_TOKEN = 'accessToken';
 
